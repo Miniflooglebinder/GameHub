089fbb33b7a9c9041f67c0d2fa8b33e2a05a7a67
Seth <seth.horgan@gmail.com>
2025-05-01 11:22:54 +0800

    Built a NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import NavBar from "@/components/NavBar";
+
 function App() {
   return (
     <div className="grid grid-cols-1 grid-rows-1 lg:grid-cols-2 lg:grid-rows-2">
-      <div className="w-full bg-red-500 lg:col-span-2">Nav</div>
+      <div className="w-full lg:col-span-2">
+        <NavBar />
+      </div>
       <div className="hidden lg:block bg-amber-400">Aside</div>
       <div className="bg-blue-500">Main</div>
     </div>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+  return (
+    <nav className="flex items-center gap-4">
+      <p className="text-5xl font-semibold">GH</p>
+      <p>NavBar</p>
+    </nav>
+  );
+};
+
+export default NavBar;
