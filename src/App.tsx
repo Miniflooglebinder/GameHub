78c9e04e9a0e4689654e9dab3cc14b6aca1cc3ab
Seth <seth.horgan@gmail.com>
2025-05-01 11:13:01 +0800

    Built a responsive grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
-
 function App() {
   return (
-    <>
-      <Button variant={"outline"}>Click me</Button>
-    </>
+    <div className="grid grid-cols-1 grid-rows-1 lg:grid-cols-2 lg:grid-rows-2">
+      <div className="w-full bg-red-500 lg:col-span-2">Nav</div>
+      <div className="hidden lg:block bg-amber-400">Aside</div>
+      <div className="bg-blue-500">Main</div>
+    </div>
   );
 }
 
