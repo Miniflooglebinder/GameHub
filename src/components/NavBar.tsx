fffda624b1aeadd05ca51207a565a342b404429f
Seth <seth.horgan@gmail.com>
2025-07-15 14:03:29 +0800

    Fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,13 @@
 import SearchInput from "@/components/SearchInput";
 import ThemeToggle from "@/components/ThemeToggle";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="flex items-center gap-4 p-2">
-      <p className="text-4xl font-semibold">GH</p>
+      <Link to="/" className="text-4xl font-semibold cursor-pointer select-none">
+        GH
+      </Link>
       <SearchInput />
       <ThemeToggle />
     </nav>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,19 +1,24 @@
 import { Input } from "@/components/ui/input";
 import useGameQueryStore from "@/GameQueryStore";
 import { Search } from "lucide-react";
-import { useRef } from "react";
+import { FormEvent, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
+  const navigate = useNavigate();
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
   const ref = useRef<HTMLInputElement>(null);
 
+  const handleSearch = (event: FormEvent) => {
+    event.preventDefault();
+    if (ref.current) {
+      setSearchText(ref.current.value);
+      navigate("/");
+    }
+  };
+
   return (
-    <form
-      className="w-full"
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (ref.current) setSearchText(ref.current.value);
-      }}>
+    <form className="w-full" onSubmit={handleSearch}>
       <span className="flex items-center w-full *:rounded-md *:first:rounded-r-none *:last:rounded-l-none *:border *:border-input *:first:border-r-0">
         <div className="flex items-center justify-center bg-transparent dark:bg-input/30 size-9 shadow-xs">
           <Search />
